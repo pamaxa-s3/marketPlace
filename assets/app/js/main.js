18ecc6a13083b4e29cd39a04ca5c28b5f368e959
Roman Strynzha <pamaxa-s3@ukr.net>
2019-12-04 19:31:38 +0200

    start project

---
+++ assets/app/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+
+$('.rate-star').rateYo({
+    rating: 4.5,
+    starWidth: '12px',
+    readOnly: true
+});
+
+$('.products-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+});
+
+$('.js-range-slider').ionRangeSlider({
+    type: 'double',
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: '$'
+});
+
+var mixer = mixitup('.products__inner-box');
+
+});
