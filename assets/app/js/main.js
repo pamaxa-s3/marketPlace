493cc2dcf07b042873f8fef67e9a3242a71d7dac
Roman Strynzha <pamaxa-s3@ukr.net>
2019-12-05 22:14:23 +0200

    added create-slider & features

--- assets/app/js/main.js
+++ assets/app/js/main.js
@@ -1,27 +1,25 @@
 $(function(){
 
-$('.rate-star').rateYo({
-    rating: 4.5,
-    starWidth: '12px',
-    readOnly: true
-});
+// $('.rate-star').rateYo({
+//     rating: 4.5,
+//     starWidth: '12px',
+//     readOnly: true
+// });
 
-$('.products-slider__inner').slick({
-    dots: true,
+$('.create-slider__inner').slick({
     arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4
+    autoplay: true,
 });
 
-$('.js-range-slider').ionRangeSlider({
-    type: 'double',
-    min: 0,
-    max: 1000,
-    from: 0,
-    to: 600,
-    prefix: '$'
-});
+// $('.js-range-slider').ionRangeSlider({
+//     type: 'double',
+//     min: 0,
+//     max: 1000,
+//     from: 0,
+//     to: 600,
+//     prefix: '$'
+// });
 
-var mixer = mixitup('.products__inner-box');
+// var mixer = mixitup('.products__inner-box');
 
 });
