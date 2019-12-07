4792f008508118b9f7da667112a441cff7177009
Roman Strynzha <pamaxa-s3@ukr.net>
2019-12-07 11:51:17 +0200

    added product-slider

--- assets/app/js/main.js
+++ assets/app/js/main.js
@@ -1,25 +1,51 @@
-$(function(){
+$(function () {
 
-// $('.rate-star').rateYo({
-//     rating: 4.5,
-//     starWidth: '12px',
-//     readOnly: true
-// });
 
-$('.create-slider__inner').slick({
-    arrows: false,
-    autoplay: true,
-});
+    $('.rate-star').rateYo({
+        rating: 4.5,
+        starWidth: '17px',
+        readOnly: true,
+        ratedFill: '#ffc000',
+        normalFill: '#d5d5d5',
+    });
 
-// $('.js-range-slider').ionRangeSlider({
-//     type: 'double',
-//     min: 0,
-//     max: 1000,
-//     from: 0,
-//     to: 600,
-//     prefix: '$'
-// });
 
-// var mixer = mixitup('.products__inner-box');
+    $('.create-slider__inner').slick({
+        arrows: false,
+        autoplay: true,
+        dots: false,
+    });
+
+
+    $('.featured-item__slider').slick({
+        arrows: false,
+        dots: false,
+    });
+
+
+    $('.arow-prev').on('click', function (event) {
+        event.preventDefault();
+
+        $('.featured-item__slider').slick('slickPrev')
+    });
+
+
+    $('.arow-next').on('click', function (event) {
+        event.preventDefault();
+
+        $('.featured-item__slider').slick('slickNext')
+    });
+
+    
+    // $('.js-range-slider').ionRangeSlider({
+    //     type: 'double',
+    //     min: 0,
+    //     max: 1000,
+    //     from: 0,
+    //     to: 600,
+    //     prefix: '$'
+    // });
+
+    // var mixer = mixitup('.products__inner-box');
 
 });
